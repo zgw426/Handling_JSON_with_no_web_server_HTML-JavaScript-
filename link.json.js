9d4137dc229da49b3ec49caafff550288d056449
suo-takefumi <zgw426@gmail.com>
2021-03-11 21:17:21 +0900

    Create link.json.js

---
+++ link.json.js
@@ -0,0 +1,41 @@
+let list = [
+  {
+    "columnA": "0番目カラムAの値",
+    "columnB": "0番目カラムBの値",
+    "columnC": 300,
+    "columnD": ["D001","D002","D003"],
+    "columnE": [
+        "E00001",
+        "E00002",
+        "E00003"
+      ],
+    "columnF": ["HHH","FFF","AAA","BBB","CCC","DDD","CCC","CCC"],
+  },
+  {
+    "columnA": "1番目カラムAの値",
+    "columnB": "1番目カラムBの値",
+    "columnC": 200,
+    "columnD": ["D011","D012"],
+    "columnE": [
+        "E00011",
+        "E00012",
+        "E00013",
+        "E00014",
+        "E00015",
+        "E00016"
+      ],
+    "columnF": ["DDD","EEE","FFF","GGG","HHH","III","JJJ"],
+  },
+  {
+    "columnA": "2番目カラムAの値",
+    "columnB": "2番目カラムBの値",
+    "columnC": 100,
+    "columnD": ["D021","D022","D023"],
+    "columnE": [
+        "E00021",
+        "E00022"
+      ],
+    "columnF": ["AAA","BBB","GGG","HHH","KKK"],
+  },
+
+];
